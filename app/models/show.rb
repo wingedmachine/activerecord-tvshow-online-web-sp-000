173870886bd26661c40154c91e85b4754faa8326
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 04:25:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,22 @@
 class Show < ActiveRecord::Base
+  def highest_rating
+  end
+
+  def most_popular_show
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
+
+  def ratings_sum
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
 end
